4d91e51e114ef52b6cb6c918ca201e3a9b21e8b6
Emil Dimitrov <edimitrov1@gmail.com>
2018-12-06 00:19:52 +0200

    Can select random valid word

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,5 +1,22 @@
 class Game
-  def initialize(name)
-    @name = name
-  end 
+  def initialize(file_name)
+    @file_name = file_name
+  end
+
+  def start
+    random_word
+  end
+
+  private
+  def load_dict
+    File.readlines(@file_name, chomp: true)
+  end
+
+  def random_word
+    valid_words = load_dict.find_all do |word| 
+      word.length >= 5 && word.length <= 12
+    end
+
+    valid_words[rand(valid_words.length)]
+  end
 end
