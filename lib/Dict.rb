3ff38ca5892da366ab1508d38cb794489a455d37
Emil Dimitrov <edimitrov1@gmail.com>
2019-08-27 20:03:52 +0300

    Make load method private

--- lib/Dict.rb
+++ lib/Dict.rb
@@ -7,19 +7,19 @@ class Dict
 
   # Selects a random valid word from the dict
   def self.random_word
-    valid_words = load_dict.find_all do |word| 
+    valid_words = self.load.find_all do |word| 
       Helpers.valid_word?(word)
     end
 
     valid_words[rand(valid_words.length)]
   end
 
+  private
+
+  attr_reader :dict
+
   def self.load
     dict = self.new(FILE_NAME)
     File.readlines(dict, chomp: true)
   end
-
-  private
-
-  attr_reader :dict
 end
