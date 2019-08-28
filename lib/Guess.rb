5309b08511010419e410bfda8f51d32b9b3a73f6
Emil Dimitrov <edimitrov1@gmail.com>
2019-08-29 00:14:50 +0300

    Remove redundnat complete method

--- lib/Guess.rb
+++ lib/Guess.rb
@@ -6,26 +6,8 @@ class Guess
   def self.make(word)
     loop do
       print "Your guess: "
-
       break if guess_success?(gets.chomp.downcase)
     end
-  end
-
-  def complete
-    input = nil
-    loop do 
-      input = ask_for_input
-      break if Helpers.valid_input?(input, @past_letters)
-    end
-
-    @past_letters << input if Helpers.valid_letter?(input, @past_letters)
-
-    if @word.has_letter?(input)
-      @word.correct_letter(input)
-      show_correct_guess(input)
-    end
-
-    @word.guessed?(input)
   end
 
   private
